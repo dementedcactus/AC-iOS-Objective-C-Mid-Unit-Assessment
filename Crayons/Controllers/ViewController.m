573b547f97e0d07b4f73562bd91ec8ab385fa513
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-05-18 13:02:13 -0400

    tableview loading objects correctly

--- Crayons/Controllers/ViewController.m
+++ Crayons/Controllers/ViewController.m
@@ -7,28 +7,48 @@
 //
 
 #import "ViewController.h"
+#import "Course.h"
+#import "Crayon.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) NSArray<Course *> *courses;
+@property (strong, nonatomic) NSMutableArray<Crayon *> *crayons;
 
 @end
 
 @implementation ViewController
 
+NSString *cellID = @"cellID";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self setupView];
     self.navigationItem.title = @"Crayons";
     self.navigationController.navigationBar.prefersLargeTitles = YES;
-    [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:@"cellID"];
+    [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:cellID];
 }
 
+- (void)setupView{
+    self.crayons = NSMutableArray.new;
+    [self.crayons addObjectsFromArray:Crayon.allTheCrayons];
+    
+}
+
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    return self.crayons.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID" forIndexPath:indexPath];
-    cell.backgroundColor = UIColor.redColor;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
+    
+    Crayon *crayon = self.crayons[indexPath.row];
+    
+    //cell.backgroundColor = UIColor.redColor;
+    cell.textLabel.text = crayon.name;
+    
+    
     return cell;
 }
 
